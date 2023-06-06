33fa7144efdea6ce5e30091fb787f77cbf5aa445
StreamPay <streamprotocol@protonmail.com>
2023-06-06 11:26:03 +0300

    first commit

---
+++ src/utils/contracts-config.js
@@ -0,0 +1,5 @@
+
+  export const contractAddress = "0x734a9F281FbD0C657132A51d9a32dC6349817d8E"
+  export const ownerAddress = "0x603fEc266a01F4297993d989aa19bb50D2830242"
+  export const networkDeployedTo = "1337"
+  
